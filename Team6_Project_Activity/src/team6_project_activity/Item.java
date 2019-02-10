b018006ba0d85503f43ea7f45874dc7bb3634ab4
1895126 <1895126@E21031.CEGEPGIM.LCL>
2019-02-10 08:59:36 -0500

    declare iteml class

---
+++ Team6_Project_Activity/src/team6_project_activity/Item.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6_project_activity;
+
+/**
+ *
+ * @author 1895126
+ */
+public class Item {
+    
+    double shippingWeight;
+    String description;
+    
+    public void getPriceForQuantity(){
+        
+    }
+    
+    public void getTax(){
+        
+    }
+    public void inStoke(){
+        
+    }
+}
