2826894a2447eb5c00820187041de993216dac59
1895198 <1895198@E21034.CEGEPGIM.LCL>
2019-02-10 09:18:02 -0500

    Shriya -- uploading Payment.java file

---
+++ Team6_Project_Activity/src/team6_project_activity/Payment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6_project_activity;
+
+/**
+ *
+ * @author 1895198
+ */
+public class Payment
+{
+    float amount;
+    Order o;
+    
+    public void setAmount(float amount)
+    {
+        this.amount=amount;
+    }
+    
+    public float getAmount()
+    {
+        return this.amount;
+    }
+}
