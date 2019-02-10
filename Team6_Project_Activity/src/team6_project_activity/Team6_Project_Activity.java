8e1d06c97a660d190bfc992db655fc7416b8df88
Ravi Teja <ravi.avanigadda95@gmail.com>
2019-02-10 08:41:13 -0500

    Initializing project files

---
+++ Team6_Project_Activity/src/team6_project_activity/Team6_Project_Activity.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6_project_activity;
+
+/**
+ *
+ * @author 1895212
+ */
+public class Team6_Project_Activity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
