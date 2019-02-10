fcbc4a56df26942a9bd485e22ee9b1e3492edc41
1894475 <1894475@E21056.CEGEPGIM.LCL>
2019-02-10 09:40:06 -0500

    Sruthi-uploading Check & Credit(java class)

---
+++ Team6_Project_Activity/src/team6_project_activity/Check.java
@@ -0,0 +1,57 @@
+
+package team6_project_activity;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1894475
+ */
+public class Check 
+{
+    String name;
+    String bankID;
+
+    public Check(String name,String bankID)
+    {
+        this.name=name;
+        this.bankID=bankID;
+    }
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+    public String getname()
+    {
+        return this.name;
+       
+    }
+    public void setbankID(String bankID)
+    {
+        this.bankID=bankID; 
+    }
+    public String getbankID()
+    {
+        return this.bankID;
+    }
+    public void authorized()
+    {
+        
+    }
+
+            
+            
+       
+            
+            
+            
+            
+            
+            
+    
+}
