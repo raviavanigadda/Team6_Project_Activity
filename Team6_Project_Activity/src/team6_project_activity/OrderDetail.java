2bd0d3cbffe1115878bb29d0125c373705c70e71
1895126 <1895126@E21031.CEGEPGIM.LCL>
2019-02-10 08:53:35 -0500

    declare orderdetail class

---
+++ Team6_Project_Activity/src/team6_project_activity/OrderDetail.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6_project_activity;
+
+/**
+ *
+ * @author 1895126
+ */
+public class OrderDetail {
+    
+    int quantity;
+    String taxStatus;
+    
+    public void calcWeight(){
+        
+    }
+    
+}
